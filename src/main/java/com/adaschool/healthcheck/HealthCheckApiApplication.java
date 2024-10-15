d770cc171f6abb65f2e9b2f15f3333339e0ce5c0
jgarcia-2019121 <jgarcia-2019121@kinal.edu.gt>
2024-10-14 22:05:30 -0600

    Initial commit with HealthController

---
+++ src/main/java/com/adaschool/healthcheck/HealthCheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.adaschool.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adaschool/healthcheck/HealthCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaschool.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
